ba6744f63b2047f6f0ec682e3e33fdbb89af9f19
imansmallapple <alf138540fun@gmail.com>
2024-03-03 04:20:16 +0100

    JS lect1

---
+++ javascript.js
@@ -0,0 +1,11 @@
+console.log(23 + 97);
+alert((4 + 6 + 9) / 77);
+let a = 10;
+console.log(a);
+
+let b = 7 * a;
+console.log(b);
+
+let max = 57;
+let actual = max - 13;
+let percentage = actual / max;
